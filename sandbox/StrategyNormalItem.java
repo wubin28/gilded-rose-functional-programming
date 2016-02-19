080a618b131c70d7a13a772bd611c2de913582d6
frog_0548DA31D5 <frog@cyber-dojo.org>
2016-02-19 14:05:48 +0000

    30

--- sandbox/StrategyNormalItem.java
+++ sandbox/StrategyNormalItem.java
@@ -1,22 +1,12 @@
 public class StrategyNormalItem implements StrategyItem{
 
     public void handle(Item item){
-        calculateQualityBeforeDecreasingSellIn(item);
-        decreaseSellIn(item);
-        calculateQualityAfterDecreasingSellIn(item);
-    }
-
-    protected void calculateQualityBeforeDecreasingSellIn(Item item) {
         if (item.quality > 0) {                    
             item.quality -= 1;
         }
-    }
 
-    protected void decreaseSellIn(Item item) {
         item.sellIn -= 1;            
-    }
 
-    protected void calculateQualityAfterDecreasingSellIn(Item item) {
         if (item.sellIn < 0) {
      
             if (item.quality > 0) {  
