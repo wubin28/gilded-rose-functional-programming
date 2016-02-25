7dec0fc7c34e79c28300b814fb8b32fa89d8b52f
frog_0548DA31D5 <frog@cyber-dojo.org>
2016-02-25 23:50:08 +0000

    83

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -241,12 +241,11 @@ public class GildedRoseTest {
             new Item("Conjured", 12, 45)
         );
 
-        Stream<Item> itemStream = items.stream()
-            .filter(GildedRose.isBackstagePasses())
-            .filter(GildedRose.isSellInNoMoreThan0())
-            .map(GildedRose.generateUpdatedBackstagePasses());
-
-        List<Item> itemsUpdated = itemStream.collect(Collectors.toList());
+        List<Item> itemsUpdated = 
+            GildedRose.updateItemsQualityAndSellIn(items, 
+                GildedRose.isBackstagePasses(), 
+                GildedRose.isSellInNoMoreThan0(),
+                GildedRose.generateUpdatedBackstagePasses());
 
         assertEquals(1, itemsUpdated.size());
         assertEquals(-1, itemsUpdated.get(0).sellIn);
